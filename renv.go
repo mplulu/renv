9b66cf95681f47e71e20558b90e766403e38cc3d
Gia Dang <hgia1234@gmail.com>
2021-08-03 16:01:59 +0700

    will load sample for missing env

--- renv.go
+++ renv.go
@@ -34,10 +34,54 @@ func Parse(env string, v interface{}) {
 		}
 	}
 
-	raw, err := ioutil.ReadFile(fileName)
-	if err != nil {
-		panic(err)
+	// check sample
+	var sampleData map[string]interface{}
+	fileNameSample := "./.env.sample.yaml"
+	if _, err := os.Stat(fileNameSample); os.IsNotExist(err) {
+		// intent blank
+	} else {
+		raw, err := ioutil.ReadFile(fileNameSample)
+		if err != nil {
+			panic(err)
+		}
+		err = yaml.Unmarshal(raw, &sampleData)
+		if err != nil {
+			panic(err)
+		}
 	}
+	var raw []byte
+	var err error
+	// merge with sample
+	if len(sampleData) > 0 {
+		var configData map[string]interface{}
+		raw, err = ioutil.ReadFile(fileName)
+		if err != nil {
+			panic(err)
+		}
+		err = yaml.Unmarshal(raw, &configData)
+		if err != nil {
+			panic(err)
+		}
+
+		mergedData := map[string]interface{}{}
+		for key, value := range sampleData {
+			mergedData[key] = value
+		}
+		for key, value := range configData {
+			mergedData[key] = value
+		}
+		raw, err = yaml.Marshal(mergedData)
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		raw, err = ioutil.ReadFile(fileName)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	// parse to v
 	err = yaml.Unmarshal(raw, v)
 	if err != nil {
 		panic(err)
